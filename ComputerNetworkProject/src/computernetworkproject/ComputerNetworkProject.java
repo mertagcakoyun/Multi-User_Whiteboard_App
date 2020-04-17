dfab5568f0db33da71bfd974f435c9fd06e7fa74
Mert <mert.agcakoyun@stu.fsm.edu.tr>
2020-04-17 13:19:55 +0300

    Project files created.

---
+++ ComputerNetworkProject/src/computernetworkproject/ComputerNetworkProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computernetworkproject;
+
+/**
+ *
+ * @author mertagcakoyun <contact : mert.agcakoyun21@gmail.com>
+ */
+public class ComputerNetworkProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
